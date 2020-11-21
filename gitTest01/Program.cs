9d6366e044501bc7f0b473d49a9e27997d5a8597
Maria Osipova <osipova74m@gmail.com>
2020-11-21 18:11:21 +0200

    initial commit

---
+++ gitTest01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitTest01
+{
+    class Program
+    {
+        public static int Version { get; set; } = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Application version is: {Version}");
+            Console.ReadLine();
+        }
+    }
+}
