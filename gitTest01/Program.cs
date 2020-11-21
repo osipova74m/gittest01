21872923dad383563a4df5329f4da8fe790deea9
Maria Osipova <osipova74m@gmail.com>
2020-11-21 18:12:37 +0200

    added sin 30

--- gitTest01/Program.cs
+++ gitTest01/Program.cs
@@ -8,10 +8,15 @@ namespace gitTest01
 {
     class Program
     {
-        public static int Version { get; set; } = 1;
+        public static int Version { get; set; } = 5;
         static void Main(string[] args)
         {
             Console.WriteLine($"Application version is: {Version}");
+
+            var sin = Math.Sin(30.0);
+            Console.WriteLine($"Sin 30 = {sin}");
+                 
+
             Console.ReadLine();
         }
     }
